d6253c9c5b54bbeeb5185a38d87b26d8a2494fe0
Duc Ta <tminhduc2811@gmail.com>
2020-06-03 16:19:37 +0700

    Initialize project

---
+++ src/test/kotlin/com/ducta/taskmanagement/TaskManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ducta.taskmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
